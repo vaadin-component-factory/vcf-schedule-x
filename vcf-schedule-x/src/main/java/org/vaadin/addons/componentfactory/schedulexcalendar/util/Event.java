0ee06f7b0fef4c156aeecdc73e3954ea399c1334
Paola De Bartolo <paola.debartolo@gmail.com>
2025-04-15 17:42:51 -0300

    fix: add missing event properties

--- vcf-schedule-x/src/main/java/org/vaadin/addons/componentfactory/schedulexcalendar/util/Event.java
+++ vcf-schedule-x/src/main/java/org/vaadin/addons/componentfactory/schedulexcalendar/util/Event.java
@@ -26,6 +26,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 /**
+ * Calendar event definition.
+ * 
  * @see <a href="https://schedule-x.dev/docs/calendar/events">Events documentation</a>
  */
 @SuppressWarnings("serial")
@@ -60,8 +62,16 @@ public class Event implements Serializable {
    */
   private String calendarId;
 
-  // TODO complete
+  /**
+   * Optional configuration for the event.
+   */
+  private EventOptions options;
 
+  /**
+   * Optional custom content to render in different views.
+   */
+  private EventCustomContent customContent;
+ 
   @Override
   public int hashCode() {
     return Objects.hash(id);
@@ -84,10 +94,12 @@ public class Event implements Serializable {
     js.put("id", id);
     js.put("start", start);
     js.put("end", end);
+   
     Optional.ofNullable(title).ifPresent(value -> js.put("title", value));
     Optional.ofNullable(description).ifPresent(value -> js.put("description", value));
     Optional.ofNullable(location).ifPresent(value -> js.put("location", value));
     Optional.ofNullable(calendarId).ifPresent(value -> js.put("calendarId", value));
+    
     if (people != null && !people.isEmpty()) {
       JsonArray jsonPeople = Json.createArray();
       for (int i = 0; i < people.size(); i++) {
@@ -95,7 +107,51 @@ public class Event implements Serializable {
       }
       js.put("people", jsonPeople);
     }
+    
+    if (options != null) {
+      js.put("_options", options.toJson());
+    }
+
+    if (customContent != null) {
+      JsonObject jsonCustomContent = Json.createObject();
+      Optional.ofNullable(customContent.getTimeGrid()).ifPresent(v -> jsonCustomContent.put("timeGrid", v));
+      Optional.ofNullable(customContent.getDateGrid()).ifPresent(v -> jsonCustomContent.put("dateGrid", v));
+      Optional.ofNullable(customContent.getMonthGrid()).ifPresent(v -> jsonCustomContent.put("monthGrid", v));
+      Optional.ofNullable(customContent.getMonthAgenda()).ifPresent(v -> jsonCustomContent.put("monthAgenda", v));
+      js.put("_customContent", jsonCustomContent);
+    }
+    
     return js.toJson();
   }
 
+  @Setter
+  @Getter
+  public static class EventOptions implements Serializable {
+    private Boolean disableDND;
+    private Boolean disableResize;
+    private List<String> additionalClasses;
+
+    public JsonObject toJson() {
+      JsonObject json = Json.createObject();
+      Optional.ofNullable(disableDND).ifPresent(value -> json.put("disableDND", value));
+      Optional.ofNullable(disableResize).ifPresent(value -> json.put("disableResize", value));
+      if (additionalClasses != null && !additionalClasses.isEmpty()) {
+        JsonArray jsonArray = Json.createArray();
+        for (int i = 0; i < additionalClasses.size(); i++) {
+          jsonArray.set(i, additionalClasses.get(i));
+        }
+        json.put("additionalClasses", jsonArray);
+      }
+      return json;
+    }
+  }
+
+  @Setter
+  @Getter
+  public static class EventCustomContent implements Serializable {
+    private String timeGrid;
+    private String dateGrid;
+    private String monthGrid;
+    private String monthAgenda;
+  }
 }
